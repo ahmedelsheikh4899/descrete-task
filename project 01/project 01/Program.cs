194f1e27f8c49edf2eb5c28ad43dafe63337f49c
ahmedelsheikh4899 <121239262+ahmedelsheikh4899@users.noreply.github.com>
2022-12-22 15:43:28 +0200

    Add files via upload

---
+++ project 01/project 01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, syco, zoro, bar, fpy;
+
+            Console.Write("\n\n");
+            Console.Write("Find the prime numbers within a range of numbers:\n");
+            Console.Write("---------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input first number  : ");
+           bar = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input second number : ");
+            fpy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The prime numbers between {0} and {1} are : \n", bar, fpy);
+
+
+            for (num = bar; num <= fpy; num++)
+            {
+                zoro = 0;
+
+                for (syco = 2; syco <= num / 2; syco++)
+                {
+                    if (num % syco == 0)
+                    {
+                        zoro++;
+                        break;
+                    }
+                }
+
+                if (zoro == 0 && num != 1)
+                    Console.Write("{0} ", num);
+            }
+            Console.Write("\n");
+        }
+
+
+    }
+    
+}
---
+++ project 02/project 02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k,c, sum  ,m, x;
+           
+
+            Console.Write("\n\n");
+            Console.Write("Find perfect numbers:\n");
+            Console.Write("--------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input the first number (n1) : ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the second number (n2) : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("the list of the perfect numbers from n1 to n2   : ");
+            
+
+
+
+            for (k = m; k <= x; k++)
+            {
+                c = 1;
+                sum = 0;
+                while (c < k)
+                {
+                    if (k % c == 0)
+                        sum = sum + c;
+                    c++;
+                }
+                if (sum == k)
+                    Console.Write("{0} ", k);
+            }
+            Console.Write("\n");
+
+
+
+
+
+        }
+    }
+}
